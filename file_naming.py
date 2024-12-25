8b2cf4338d41cce50c1f6eb4e771a1a593cd55de
Abu Saleh Musa Miah <abusalehcse.ru@gmail.com>
2024-12-25 11:07:00 +0900

    Update and rename file_naming to file_naming.py

--- file_naming.py
+++ file_naming.py
@@ -35,3 +35,22 @@ for f in sorted(files):
         new_file_path = os.path.join(save_directory, new_file_name)
         shutil.copy(f, new_file_path)
         print(f"Saved {file_name} as {new_file_name} in {save_directory}")
+
+
+
+%%%
+path='D:/JOB/Eyes Japan/Pakrinson Project/Company_Data//_1*.*'
+
+files = glob.glob(path)
+print(files)
+for f in sorted(files):
+    #(f.replace('\\', '/')).split('/')[-5].split('_')[1]
+    #print(f)
+    subject=(f.replace('\\', '/')).split('/')[-2]
+    #print(subject)
+    task_string=(f.replace('\\', '/')).split('/')[-1].split('_')[1]
+    #print(task_string)
+    tm=task_string[:2]
+
+
+%%%
